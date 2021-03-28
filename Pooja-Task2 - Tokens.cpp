685c611a5c1c0d39cbdcdb40349a3668258d6846
poojag19 <74090587+poojag19@users.noreply.github.com>
2021-03-28 16:19:15 +0530

    Task1 and Task2
    Pooja completed two assignments

---
+++ Pooja-Task2 - Tokens.cpp
@@ -0,0 +1,13 @@
+// Pooja Task2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double no1, no2, pof1n2;
+    cout << "Enter two numbers: ";
+    cin >> no1 >> no2;
+    pof1n2 = no1 * no2;  
+    cout << "ProductValue = " << pof1n2;    
+    return 0;
+}
